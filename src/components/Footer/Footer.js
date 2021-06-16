e786fec99d9899796bf3928633ade01cd7647ed8
webdev0831 <fullstack831@gmail.com>
2021-06-16 23:09:44 +0300

    changed: andrei

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
           rel="noopener noreferrer"
           target="_blank"
         >
-          Oleg Khurtin
+          Andrei Abdulin
         </a>{" "}
       </h2>
     </div>
